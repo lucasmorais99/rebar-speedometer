d46503b3899a4ef559f66a71ee171639704211bd
Lucas Morais <44041139+lucasmorais99@users.noreply.github.com>
2025-02-14 12:12:19 -0300

    Update index.ts
    Changed webview from 'page' to 'overlay'

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const Rebar = useRebar();
 // Show up the speedometer when the player enters the vehicle.
 alt.on('playerEnteredVehicle', (player: alt.Player, vehicle: alt.Vehicle, seat: number) => {
     if (seat === 1) { // Check if the player is the driver
-        Rebar.player.useWebview(player).show('Speedometer', 'page');
+        Rebar.player.useWebview(player).show('Speedometer', 'overlay');
         Rebar.player.useWebview(player).emit('updateSpeed');
         Rebar.player.useWebview(player).unfocus();
     }
